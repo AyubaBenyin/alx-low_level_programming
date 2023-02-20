63ce971f750423f3200eada0a133646e0afff4b1
AyubaBenyin <ayubayawbenyin@yahoo.com>
2023-02-20 13:21:47 -0800

    print_combo5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 100; i++)
+	{
+		for (j = i; j < 100; j++)
+		{
+			putchar('0' + (i / 10));
+			putchar('0' + (i % 10));
+			putchar(' ');
+			putchar('0' + (j / 10));
+			putchar('0' + (j % 10));
+			putchar(';');
+			putchar(' ');
+		}
+	}
+
+	return (0);
+}
