3de66ddfaa7bb19dce0fcdf38c12d8b937ec1c9d
AyubaBenyin <ayubayawbenyin@yahoo.com>
2023-02-21 09:04:13 -0800

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,18 +1,18 @@
-#include <unistd.h>
+#ifndef main_h
+#define main_h
 
-/**
- * main - Entry point
- * 
- * Return: Always  0 (Success).
- */
-int main(void)
-{
-        char word[8] = "_putchar";
-        int a;
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
 
-        for (a = 0; a < 9; a++)
-                _putchar(word[a]);
-        _putchar('\n');
-
-        return (0);
-}
+#endif
