f341661e37c519d7c59bf68ccc8a8a6cefa6de85
AyubaBenyin <ayubayawbenyin@yahoo.com>
2023-02-21 11:08:35 -0800

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _abs - a function that computes the absolute value of an integer
+ * @ij: integer input
+ * Return: Absolute value of ab
+ */
+int _abs(int ij)
+{
+	return (ij * ((ij > 0) - (ij < 0)));
+}
