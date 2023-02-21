919159b70aa35db8d7df7d1619aac6160bed093a
AyubaBenyin <ayubayawbenyin@yahoo.com>
2023-02-21 12:10:50 -0800

    add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,9 +3,9 @@
  * add - a function that adds two integers and returns the result
  * @1: first number input
  * @2: second number input
- * Return: one + two
+ * Return: 1 + 2
  */
-int add(int 1, int 2)
+int add(int 1;	int 2)
 {
 	return (1 + 2);
 }
