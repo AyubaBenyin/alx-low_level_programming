a709106080eb70a0dbb292409858b0e1fd3ea4da
AyubaBenyin <ayubayawbenyin@yahoo.com>
2023-03-01 13:56:08 -0800

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,12 +1,6 @@
 #include <stdio.h>
-#include "main.h"
 
-/**
- * Add one line to this code 
- * so that the program prints a[2] = 98, followed by a new line.
- * Return: Aways 0 (Success)
- */
-
+/* adding any of these lines makes code to print "a[2] = 98" at output */
 int main(void)
 {
 	int n;
@@ -15,16 +9,10 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
-	/*
-	 * write your line of code here...
-	 * Remember:
-	 * - you are not allowed to use a
-	 * - you are not allowed to modify p
-	 * - only one statement
-	 * - you are not allowed to code anything else than this line of code
-	 */
-	*(p + 5) = 98;
-	/* ...so that this prints 98\n */
-	printf("a[2] = %d\n", a[2]);
+
+	p[5] = 98; //OR *(p + 5) = 98;
+
+	printf("a[2] = %d\n", a[2]); //prints a[2] = 98
+
 	return (0);
 }
