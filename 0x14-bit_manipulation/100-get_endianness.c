83991defd11fbf4f2a5408b4e00ccdc4a28f21cb
AyubaBenyin <ayubayawbenyin@yahoo.com>
2023-04-09 06:26:40 -0700

    100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * get_endianness - A function that checks endianness
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	int num = 1;
+	char *c = (char *)&num;
+
+	if (*c == 1)
+	{
+		return 1;
+	} 
+	else
+	{
+		return (0);
+	}
+}
