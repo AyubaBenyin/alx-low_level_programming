6700388f1b1e5ac588aa817793f9f7f4555a55c1
AyubaBenyin <ayubayawbenyin@yahoo.com>
2023-04-09 12:30:34 -0700

    1-create_file.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,19 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+/**
+ * read_textfile - A function that reads a text file and prints
+ * to the POSIX STDOUT
+ * @filename: The filename to open
+ * @letters: The number of letters to read and print
+ * Return: The number of letters read and printed, or 0 on failure
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
+}
