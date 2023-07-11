2037954e683456e5cca8bf4ba141ad8dbeb72831
AyubaBenyin <ayubayawbenyin@yahoo.com>
2023-07-11 02:24:34 -0700

    updated code

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,13 @@ int create_file(const char *filename, char *text_content)
 	for (len = 0; text_content[len]; len++)
 		;
 	wr_stat = write(new_file, text_content, len);
-	if (close(new_file) == -1)
+	if (wr_stat == -1)
+	{
+		close(new_file);
 		return (-1);
-	return (wr_stat == -1 ? -1 : 1);
+	}
+
+	close(new_file);
+
+	return (1);
 }
