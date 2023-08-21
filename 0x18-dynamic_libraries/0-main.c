caba4fae0660552f523d6d896ec7bdd1a560dc0a
AyubaBenyin <ayubayawbenyin@yahoo.com>
2023-08-21 16:47:44 -0700

    push them up

---
+++ 0x18-dynamic_libraries/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for ALx School students.
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+	printf("%d\n", _strlen("ALx"));
+	return (EXIT_SUCCESS);
+}
---
+++ 0x18-dynamic_libraries/README.md
@@ -0,0 +1,2 @@
+libdynamic.so, main.h
+1-create_dynamic_lib.sh
